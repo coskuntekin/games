fc604f9017b4103460bde0cc9d94255a9f443fa2
coskuntekin <coskuntekin@msn.com>
2024-08-30 14:16:35 +0800

    refactor: Update MainMenu scene with improved text styling and formatting

--- src/scenes/MainMenu.js
+++ src/scenes/MainMenu.js
@@ -1,28 +1,24 @@
-import { Scene } from 'phaser';
+import { Scene } from "phaser";
 
-export class MainMenu extends Scene
-{
-    constructor ()
-    {
-        super('MainMenu');
-    }
+export class MainMenu extends Scene {
+  constructor() {
+    super("MainMenu");
+  }
 
-    create ()
-    {
-        this.add.image(512, 384, 'background');
+  create() {
+    this.add
+      .text(512, 460, "Enter to Start", {
+        fontFamily: "Arial Black",
+        fontSize: 38,
+        color: "#ffffff",
+        stroke: "#000000",
+        strokeThickness: 8,
+        align: "center",
+      })
+      .setOrigin(0.5);
 
-        this.add.image(512, 300, 'logo');
-
-        this.add.text(512, 460, 'Main Menu', {
-            fontFamily: 'Arial Black', fontSize: 38, color: '#ffffff',
-            stroke: '#000000', strokeThickness: 8,
-            align: 'center'
-        }).setOrigin(0.5);
-
-        this.input.once('pointerdown', () => {
-
-            this.scene.start('Game');
-
-        });
-    }
+    this.input.once("pointerdown", () => {
+      this.scene.start("Game");
+    });
+  }
 }
