3e55778cdde2796a439ac2c6b5d5831dfef2e8d4
coskuntekin <coskuntekin@msn.com>
2024-08-30 14:16:42 +0800

    chore: Update Vite config files for consistency and formatting

--- vite/config.dev.mjs
+++ vite/config.dev.mjs
@@ -1,17 +1,14 @@
-import { defineConfig } from 'vite';
+import { defineConfig } from "vite";
 
 export default defineConfig({
-    base: './',
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks: {
-                    phaser: ['phaser']
-                }
-            }
+  base: "./",
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          phaser: ["phaser"],
         },
+      },
     },
-    server: {
-        port: 8080
-    }
+  },
 });
